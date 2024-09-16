85e7d546aea7c817a114f1c840f6912c334cb7b3
KIU-PC <bibiashviliandria@gmail.com>
2024-09-17 02:41:04 +0400

    cities and regions fetch

---
+++ src/composables/useRegion.js
@@ -0,0 +1,30 @@
+import axios from "../interceptors/axios/index.js";
+import {ref, watch} from "vue";
+
+export const useRegion = () => {
+  const regions = ref()
+  const cities = ref()
+  const filteredCities = ref()
+  const regionIds = ref([])
+  const getRegionsAndCities = async () => {
+    try {
+      let response = await axios.get('regions')
+      regions.value = response.data
+      response = await axios.get('cities')
+      cities.value = response.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  watch(regionIds, () => {
+    filteredCities.value = cities.value.filter(c => regionIds.value.includes(String(c['region_id'])))
+  },{deep: true})
+
+  return {
+    regions,
+    filteredCities,
+    regionIds,
+    getRegionsAndCities
+  }
+}
+
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,66 @@
 <script setup>
+import {useRegion} from "../composables/useRegion.js";
+import {onMounted, ref} from "vue";
 
+const {
+  regions,
+  filteredCities,
+  regionIds,
+  getRegionsAndCities
+} = useRegion()
+
+onMounted(async () => {
+  await getRegionsAndCities()
+
+})
+
+const handleCheck = (e) => {
+  if (e.target.checked) {
+    regionIds.value.push(e.target.value)
+
+  } else {
+    regionIds.value = regionIds.value.filter(r => r !== e.target.value)
+  }
+}
+
+const show = ref(true)
 </script>
 
 <template>
-  <div>
-    home
+
+  <div v-if="regions">
+    <div v-for="(region,index) in regions" :key="index" class="flex gap-1 items-center">
+      <input @change="handleCheck" type="checkbox" :value="region.id">
+      <p v-text="region.name"/>
+    </div>
   </div>
+
+  <div v-else v-text="'loading regions'"/>
+
+
+  <Transition>
+    <div v-if="filteredCities&&filteredCities.length !== 0" class="mt-10">
+      <div v-for="(city,index) in filteredCities" :key="index" class="flex gap-1 items-center">
+        <input type="checkbox">
+        <p v-text="city.name"/>
+      </div>
+    </div>
+  </Transition>
+
+
+
+
 </template>
 
-<style scoped>
 
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
 </style>
